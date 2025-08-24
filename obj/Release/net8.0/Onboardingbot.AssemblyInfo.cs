11eb852c820af9ec119ab4be12e6c1da7cb4cf9f
Nour Eldeen <noureldeendev@gmail.com>
2025-08-24 20:31:31 +0300

    recreate fix 6

--- obj/Release/net8.0/Onboardingbot.AssemblyInfo.cs
+++ obj/Release/net8.0/Onboardingbot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Onboardingbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff062895634cdc5ba7da23c5a92fd47fe9ec2cf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Onboardingbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Onboardingbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
